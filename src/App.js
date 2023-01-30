1f9b6ab26ba6161de8b0d267f0ee984d085289b8
Erkinai31 <107002927+Erkinai31@users.noreply.github.com>
2023-01-30 15:28:25 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Login } from "components/Login";
 import { SignUp } from "components/SignUp";
 import MainSpace from "components/MainSpace/MainSpace";
@@ -9,13 +9,13 @@ import MainSpace from "components/MainSpace/MainSpace";
 function App() {
   return (
     <div>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/MovieSpace-Project" element={<Login />} />
           <Route path="/signUp" element={<SignUp/>} />
           <Route path="/mainSpace" element={<MainSpace />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
